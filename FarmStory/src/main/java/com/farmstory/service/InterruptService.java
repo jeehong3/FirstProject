db15303869c41bfa6ea67fc94511653489056e26
Administrator <Administrator@MSDN-SPECIAL>
2018-07-19 15:30:30 +0900

    1

--- FarmStory/src/main/java/com/farmstory/service/InterruptService.java
+++ FarmStory/src/main/java/com/farmstory/service/InterruptService.java
@@ -5,16 +5,16 @@ import java.util.List;
 import com.farmstory.vo.Interrupt;
 
 public class InterruptService {
-
-	public void write(Interrupt interrupt) {
-	}
-
-	public List<Interrupt> findBoards() {
-		return null;
-	}
-
-	public Interrupt getInterruptDetail(int boardNo) {
-		return null;
-	}
+//
+//	public void write(Interrupt interrupt) {
+//	}
+//
+//	public List<Interrupt> findBoards() {
+//		return null;
+//	}
+//
+//	public Interrupt getInterruptDetail(int boardNo) {
+//		return null;
+//	}
 
 }
