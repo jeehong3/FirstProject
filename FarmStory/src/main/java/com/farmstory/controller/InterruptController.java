2f6189b30ad68f1304e60d58e944abf51df7e392
Administrator <Administrator@MSDN-SPECIAL>
2018-07-19 15:19:22 +0900

    1

--- FarmStory/src/main/java/com/farmstory/controller/InterruptController.java
+++ FarmStory/src/main/java/com/farmstory/controller/InterruptController.java
@@ -1,20 +1,61 @@
 package com.farmstory.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.farmstory.service.InterruptService;
-import com.farmstory.service.noticeService;
-import com.farmstory.vo.Ann;
 import com.farmstory.vo.Interrupt;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class InterruptController {
 
 	@Autowired
-	@Qualifier(value = "interruptService")
+	@Qualifier("interruptService")
 	private InterruptService interruptService;
+
+	@RequestMapping(value = "/list.action", method = GET)
+	public String showListView(Model model) {
+
+		List<Interrupt> boards = interruptService.findBoards();
+
+		model.addAttribute("boards", boards);
+
+		return "board/interrupt/list";
+	}
+
+	@RequestMapping(value = "/detail.action", method = GET)
+	public String showDetailView(int boardNo, Model model) {
+
+		Interrupt board = (Interrupt) interruptService.getInterruptDetail(boardNo);
+
+		model.addAttribute("board", board);
+		return "board/interrupt/detail";
+	}
 	
+	@GetMapping(value = "/sensorInsert.action")
+	@ResponseBody
+	public String helloArduino(int x, String msg) {
+		
+		System.out.printf("X : %d / MESSAGE : %s\n", x, msg);
+		
+//		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//		return String.format(
+//				"data received at %s", f.format(new Date()));
+		
+		
+		if (x < 300) {
+			return "turnon";
+		} else {
+			return "turnoff";
+		}
+	}
 }
--- FarmStory/src/main/java/com/farmstory/service/InterruptService.java
+++ FarmStory/src/main/java/com/farmstory/service/InterruptService.java
@@ -1,11 +1,20 @@
 package com.farmstory.service;
 
+import java.util.List;
+
 import com.farmstory.vo.Interrupt;
 
 public class InterruptService {
 
 	public void write(Interrupt interrupt) {
-		
+	}
+
+	public List<Interrupt> findBoards() {
+		return null;
+	}
+
+	public Interrupt getInterruptDetail(int boardNo) {
+		return null;
 	}
 
 }
