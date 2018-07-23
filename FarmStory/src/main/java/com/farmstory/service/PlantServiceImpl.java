5949d59cc01da0330272641bd6f4b15920f3713e
OnChulHoon <36250813+OnChulHoon@users.noreply.github.com>
2018-07-23 21:57:57 +0900

    # 식물 정보 관리 부분 식물 정보 DB등록 완료
    -by och

--- FarmStory/src/main/java/com/farmstory/service/PlantServiceImpl.java
+++ FarmStory/src/main/java/com/farmstory/service/PlantServiceImpl.java
@@ -1,6 +1,10 @@
 package com.farmstory.service;
 
+import java.util.List;
+
 import com.farmstory.dao.PlantDao;
+import com.farmstory.vo.Plant;
+import com.farmstory.vo.PlantImg;
 
 public class PlantServiceImpl implements PlantService {
 	
@@ -8,6 +12,18 @@ public class PlantServiceImpl implements PlantService {
 
 	public void setPlantDao(PlantDao plantDao) {
 		this.plantDao = plantDao;
+	}
+	
+	@Override
+	public void writePlantInfoWithImages(Plant plant) {
+		
+		plantDao.insertPlantInfo(plant);
+		
+		List<PlantImg> imgAttachments = plant.getAttachments();
+		for (PlantImg attachment : imgAttachments) {
+			attachment.setPlaNo(plant.getPlaNo());
+			plantDao.insertPlantInfoImages(attachment);
+		}
 	}
 	
 }
