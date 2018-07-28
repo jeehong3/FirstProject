82bbd9bc61d4b59a571abecba945283ba727320a
OnChulHoon <36250813+OnChulHoon@users.noreply.github.com>
2018-07-28 18:50:53 +0900

    # 메인 페이지 및 로고 수정 외 1. 메인 페이지 및 로고 이미지 수정 2. 로그인 인터셉터 수정
    -by och

--- FarmStory/src/main/java/com/farmstory/intercept/AuthInterceptor.java
+++ FarmStory/src/main/java/com/farmstory/intercept/AuthInterceptor.java
@@ -1,10 +1,7 @@
 package com.farmstory.intercept;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,42 +13,39 @@ public class AuthInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+		// session에 담긴 데이터를 받아온다.
 		Account account = (Account) request.getSession().getAttribute("loginuser");
-		
+		// 브라우저에서 요청된 주소를 받아온다.
 		String url = request.getRequestURI();
-		boolean redirect = false;
-		if (url.contains("/value/") || 
-			url.contains("plant_regist.action") ||
-			url.contains("diary_write.action") ||
-			url.contains("diary_list.action") 
-			&& account == null) {
-			
-			redirect = true;
+		// 반환할 redirect값을 true로 초기화한다.
+		boolean redirect = true;
+		// 로그인 여부를 체크한다.
+		if (url.contains("findTime.action") && account == null || 
+			url.contains("plant_regist.action") && account == null ||
+			url.contains("diary_write.action") && account == null ||
+			url.contains("diary_list.action") && account == null) {
+			// false를 반환하여 메인 페이지로 이동시킨다.
+			redirect = false;
 		}
 		
-		if (redirect) {
-			//request.getSession().setAttribute("checkedLogin", "empty");
+		if (!redirect) {
+			// 로그인 하지 않는 경우 메인 페이지로 이동시킨다.
 			response.sendRedirect("/farmstory/home.action?checkedLogin=empty");
 		}
-		return !redirect;//false를 반환하면 요청 처리 중단
+		// true를 반환하면 요청이 중단된다.
+		return redirect;
 	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
 		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
 	}
 	
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
 		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
 	}
 
-
-
-	
 }
